c3fd0640dcc039e3aa31a6b6ec63d14c3223d3cb
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-10 18:01:44 -0500

    header, topbar, home

---
+++ src/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css"
+import logo from "../assets/images/Screensho.jpg"
+
+export default function Header(){
+    return(
+        <div className='header'>
+            <div className="headerTitles">
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleLg"> Blog</span>
+            </div>
+            <img
+            className="headerImg"
+            src={logo}
+            />
+        </div>
+    )
+}
