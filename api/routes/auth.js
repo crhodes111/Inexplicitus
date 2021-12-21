195b9811b6877abdf92a870fdc98ad0c4eb9819a
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-21 12:03:55 -0500

    heroku time please

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -25,9 +25,6 @@ router.post("/login", async (req, res) =>{
     try{
         const user = await User.findOne({username:req.body.username})
         !user && res.status(400).json("Wrong credentials")
-        const validated = await bcrypt.compare(req.body.password, user.password)
-        !validated &&  res.status(400).json("Wrong credentials");
-
         const {password, ...others} = user._doc
         res.status(200).json(others);
     }
