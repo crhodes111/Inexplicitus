8f52f834d53f84b1843bc7e4c8932f2b1c1c670b
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-21 11:58:15 -0500

    heroku time please

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,15 +1,14 @@
 const router = require ("express").Router();
 const User = require("../../models/User");
-const bcrypt = require('bcrypt')
+
 //Register
 router.post("/register", async (req,res)=>{
     try{
-        const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(req.body.password, salt)
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: hashedPass,
+            password: req.body.password,
             
         });
         const user = await newUser.save();
