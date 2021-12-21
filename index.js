c5fcb5d51d95ab5f0e224b89e0ade0ba7a90f2ee
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-21 13:33:01 -0500

    please please

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ if (process.env.NODE_ENV === 'production') {
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
