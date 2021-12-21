40349a4830b7c769d911117ef009efb918937f61
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-21 16:27:58 -0500

    PF

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -9,7 +9,6 @@ export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = `${process.env.PUBLIC_URL}/images`;
   const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
@@ -49,7 +48,7 @@ export default function SinglePost() {
     <div className="singlePost">
       <div className="singlePostWrapper">
         {post.photo && (
-          <img src={PF + post.photo} alt="" className="singlePostImg" />
+          <img src={process.env.PUBLIC_URL + "images/" + post.photo} alt="" className="singlePostImg" />
         )}
         {updateMode ? (
           <input
