1f0f6006bb0b4ac06ca526ad53e33142f01097ce
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-21 14:54:34 -0500

    Single Post

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -9,7 +9,7 @@ export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "/images/";
+  const PF = "../../images/";
   const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
